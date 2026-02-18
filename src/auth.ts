ae5bec50efd9a0b3f4ace35f7f03a520380f186d
navtemmt <83179960+navtemmt@users.noreply.github.com>
2026-02-18 19:11:49 +0800

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,61 +1,55 @@
-// Source: https://gist.github.com/VityaSchel/77e482daf2ac688c18d39b126583fc86
-
+// FIXED: Skip getUser() pre-auth, force DC via index.ts
 import readlineSync from 'readline-sync'
-import { getUser, sendCode, signIn, getPassword, checkPassword } from './utils.js'
+import { sendCode, signIn, getPassword, checkPassword } from './utils.js'
 
 export default async function authorize() {
-  let user
-  if (process.argv[2] === '--logout') await global.api.call('auth.logOut')
-  else user = await getUser()
+  if (process.argv[2] === '--logout') {
+    await global.api.storage.removeAll()
+    console.log('Logged out successfully.')
+    process.exit(0)
+  }
 
-  if (user) return user
-  else {
-    const phone = process.env.PHONE
-    const { phone_code_hash } = await sendCode(phone)
-    const code = readlineSync.question('Код для авторизации: ')
-
-    try {
-      const signInResult = await signIn({
-        code,
-        phone,
-        phone_code_hash,
-      })
-
-      if (signInResult._ === 'auth.authorizationSignUpRequired') throw 'Аккаунт не найден'
-    } catch (error) {
-      switch (error.error_message) {
-        case 'SESSION_PASSWORD_NEEDED':
-          try {
-            await twoFA()
-          } catch (e) {
-            if (e.error_message === 'PASSWORD_HASH_INVALID') throw 'Неправильный пароль 2FA в .env файле'
-            else throw error
+  const phone = process.env.PHONE || readlineSync.question('Enter phone (+60xxxxxxxxx): ')
+  
+  console.log('Sending verification code...')
+  const { phone_code_hash } = await sendCode(phone)
+  
+  const code = readlineSync.question('Enter verification code: ')
+  
+  try {
+    await signIn({
+      phone,
+      phone_code_hash,
+      code
+    })
+  } catch (error) {
+    switch (error.error_message) {
+      case 'SESSION_PASSWORD_NEEDED':
+        try {
+          const { srp_id, current_algo, srp_B } = await getPassword()
+          const { g, p, salt1, salt2 } = current_algo
+          const { A, M1 } = await global.api.crypto.getSRPParams({
+            g, p, salt1, salt2, gB: srp_B,
+            password: process.env.TWO_FA_PASSWORD || readlineSync.question('Enter 2FA password: ')
+          })
+          await checkPassword({ srp_id, A, M1 })
+        } catch (e) {
+          if (e.error_message === 'PASSWORD_HASH_INVALID') {
+            console.log('Wrong 2FA password!')
+            return await authorize()
           }
-          return await getUser()
-
-        case 'PHONE_CODE_INVALID':
-          console.log('Неправильный код! Попробуйте еще раз')
-          return await authorize()
-
-        default:
-          throw error
-      }
+          throw e
+        }
+        break
+      case 'PHONE_CODE_INVALID':
+        console.log('Invalid code! Retrying...')
+        return await authorize()
+      default:
+        throw error
     }
   }
-}
-
-async function twoFA() {
-  const { srp_id, current_algo, srp_B } = await getPassword()
-  const { g, p, salt1, salt2 } = current_algo
-
-  const { A, M1 } = await global.api.crypto.getSRPParams({
-    g,
-    p,
-    salt1,
-    salt2,
-    gB: srp_B,
-    password: process.env.TWO_FA_PASSWORD,
-  })
-
-  await checkPassword({ srp_id, A, M1 })
+  
+  // Now safe: get user post-auth
+  const { users } = await global.api.call('users.getUsers', [{ _: 'inputUserSelf' }])
+  return { users }
 }
